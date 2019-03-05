b4dd3e449af2f0b5f0ac05550a46995e01eceb3e
tangmeng <tangmeng1993@gmail.com>
2019-03-05 23:08:37 +0800

    update data sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
-INSERT into tb_product values (1,'author','category','title','publisher',1.2,1,1,'imageUrl',1);
+DELETE FROM tb_product WHERE id = '1';
+DELETE FROM tb_supplier WHERE id = '1';
 INSERT into tb_supplier values (1, 'supplier1');
+INSERT into tb_product values (1,'author','category','title','publisher',1.2,1,1,'imageUrl',1);
