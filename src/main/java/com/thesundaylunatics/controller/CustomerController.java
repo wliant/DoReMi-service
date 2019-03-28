8ce735b261c6a5d4545d7691f62d9a4fb6420b47
wliant <tanweilian@outlook.sg>
2019-03-28 22:08:54 +0800

    swagger initial commit

--- src/main/java/com/thesundaylunatics/controller/CustomerController.java
+++ src/main/java/com/thesundaylunatics/controller/CustomerController.java
@@ -14,26 +14,44 @@ import javax.ws.rs.core.MediaType;
 import com.thesundaylunatics.model.Customer;
 import com.thesundaylunatics.service.CustomerService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @Path("customers")
 @Consumes(MediaType.APPLICATION_JSON)
-@Produces( {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+@Api(value = "Customer resource Version 1", consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
 public class CustomerController {
-	
+
 	@Autowired
 	private CustomerService customerService;
-	
+
 	@GET
+	@ApiOperation(nickname = "", value = "Service to get customers", notes = "get all customers", response = Customer.class, tags = {
+			"CustomerService" })
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Response ok", response = Customer.class),
+			@ApiResponse(code = 401, message = "Un authorized", response = Void.class) })
 	public List<Customer> listCustomers() {
 		return customerService.list();
 	}
-	
+
 	@Path("/{id}")
 	@GET
+	@ApiOperation(nickname = "", value = "Service to get customers", notes = "get all customers", response = Customer.class, tags = {
+			"CustomerService" })
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Response ok", response = Customer.class),
+			@ApiResponse(code = 401, message = "Un authorized", response = Void.class) })
 	public Customer getCustomer(@PathParam(value = "id") Long id) {
 		return customerService.get(id);
 	}
-	
+
 	@POST
+	@ApiOperation(nickname = "", value = "Service to get customers", notes = "get all customers", response = Customer.class, tags = {
+			"CustomerService" })
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Response ok", response = Customer.class),
+			@ApiResponse(code = 401, message = "Un authorized", response = Void.class) })
 	public Customer saveCustomer(Customer customer) {
 		return customerService.save(customer);
 	}
